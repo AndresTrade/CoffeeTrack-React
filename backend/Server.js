29cc84334a13aba7afd69a6ccd5463c6189ba87a
aandresv <aandres.vargas@gmail.com>
2024-01-25 14:43:47 -0500

    BackEnd Node.js con Express.js

--- backend/Server.js
+++ backend/Server.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
@@ -13,7 +14,7 @@ const db = mysql.createConnection({
 })
 
 app.get ('/', (re, res) =>{
-    return res.json("Del lado del Backend");
+    return res.json("Conexion BackEnd - MariaDb");
 })
 
 app.get ('/usuarios', (req, res)=>{
@@ -23,6 +24,38 @@ app.get ('/usuarios', (req, res)=>{
         return res.json(data);
 })
 })
+
+app.post ('/usuarios', (req, res)=>{
+    const sql = "INSERT INTO usuarios (`Nombre`, `TipoUsuario`, `Email`, `Password`, `Telefono`, `Curso`) VALUES (?)";
+    
+    const values = [
+        req.body.nombre,
+        req.body.usuario,
+        req.body.email,
+        req.body.password,
+        req.body.telefono,
+        req.body.curso
+    ]
+    db.query(sql,[values], (err, data)=>{
+        if(err){
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+//app.post ('/usuarios', (req, res)=>{
+  //  const sql = "SELECT * FROM  usuarios WHERE email = ? AND password = ?";
+  
+   // db.query(sql, [req.body.email, req.body.password], (err, data)=>{
+   //     if(err) return res.json("Se encontro un error");
+    //    if(data.length > 0 ){
+     //       return res.json("Login Exitoso")
+      //  } else {
+      //      return res.json("No tiene acceso")
+      //  }
+//})
+//})
 
 app.listen(8081, () => {
     console.log("Listening on ");
